efb7280c41138876e129c208a5519103dab7f3e6
nikitha <mnikitha365@gmail.com>
2020-06-15 16:27:21 +0530

    Detend Calculator

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+let atten = window.prompt("Enter Your Attendence Percentage")
+let sub = window.prompt("Enter Your Backlocks")
+if(atten>60&&sub<=0)
+{
+    console.log("Your allow to the exams")
+}
+    else if(atten>60&&sub>0)
+   {
+       console.log("Not allow to the exams")
+    }
+else
+{
+   
+    console.log("Detend")
+}
+
